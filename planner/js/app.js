519992755c70ab8800b790b7e029d3c6afd90e16
wipeaut <wipeautcrafter@gmail.com>
2019-06-06 22:57:49 +0200

    Started working on the design

---
+++ planner/js/app.js
@@ -0,0 +1,4 @@
+
+$(".expand-btn").click(function() {
+  $(this).toggleClass("expand");
+});
---
+++ planner/js/master.js
@@ -0,0 +1,4 @@
+// load all the libraries into the project
+
+window.$ = window.jQuery = require("jquery");
+require("bootstrap");
